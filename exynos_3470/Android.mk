505a5f3fae8c620b7ac8d8a0319bb4f47517687d
azuzu <0azuzu0@gmail.com>
2016-10-23 00:27:27 +0200

    Adding files
    Change-Id: I075a2580de22468b209088d3e7266124151f824f

--- exynos_3470/Android.mk
+++ exynos_3470/Android.mk
@@ -1,31 +1,27 @@
 LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
-samsung_required := AdvancedDisplay power liblights ril
-
-include $(call all-named-subdir-makefiles,$(samsung_required))
+include $(LOCAL_PATH)/AdvancedDisplay
+include $(LOCAL_PATH)/power
+include $(LOCAL_PATH)/liblights
+include $(LOCAL_PATH)/ril
 
 ifeq ($(TARGET_USE_LESS_BLOB_FILES),true)
-
-blob_less_dirs := exynos_omx libcamera2 libkeymaster libstagefrighthw libcsc \
-		  libswconverter libexynosutils libhwjpeg libsecurepath libv4l2 \
-		  modemloader mobicore macloader wifiloader
-#1    include $(LOCAL_PATH)/exynos_omx/Android.mk
-#1    include $(LOCAL_PATH)/libcamera2/Android.mk
-#    include $(LOCAL_PATH)/libgscaler/Android.mk
-#1    include $(LOCAL_PATH)/libkeymaster/Android.mk
-#1    include $(LOCAL_PATH)/libstagefrighthw/Android.mk
-#    include $(LOCAL_PATH)/gralloc/Android.mk
-#1    include $(LOCAL_PATH)/libcsc/Android.mk
-#    include $(LOCAL_PATH)/libhwc/Android.mk
-#1    include $(LOCAL_PATH)/libswconverter/Android.mk 
-#1    include $(LOCAL_PATH)/libexynosutils/Android.mk 
-#1    include $(LOCAL_PATH)/libhwjpeg/Android.mk 
-#1    include $(LOCAL_PATH)/libsecurepath/Android.mk   
-#1    include $(LOCAL_PATH)/libv4l2/Android.mk 
-#1    include $(LOCAL_PATH)/modemloader/Android.mk   
-#1    include $(LOCAL_PATH)/mobicore/Android.mk
-#1    include $(LOCAL_PATH)/macloader/Android.mk
-#1    include $(LOCAL_PATH)/wifiloader/Android.mk
-    include $(call all-named-subdir-makefiles,$(blob_less_dirs))
+    include $(LOCAL_PATH)/exynos_omx/Android.mk
+    include $(LOCAL_PATH)/libcamera2/Android.mk
+    #    include $(LOCAL_PATH)/libgscaler/Android.mk
+    include $(LOCAL_PATH)/libkeymaster/Android.mk
+    include $(LOCAL_PATH)/libstagefrighthw/Android.mk
+    #    include $(LOCAL_PATH)/gralloc/Android.mk
+    include $(LOCAL_PATH)/libcsc/Android.mk
+    #    include $(LOCAL_PATH)/libhwc/Android.mk
+    include $(LOCAL_PATH)/libswconverter/Android.mk 
+    include $(LOCAL_PATH)/libexynosutils/Android.mk 
+    include $(LOCAL_PATH)/libhwjpeg/Android.mk 
+    include $(LOCAL_PATH)/libsecurepath/Android.mk   
+    include $(LOCAL_PATH)/libv4l2/Android.mk 
+    include $(LOCAL_PATH)/modemloader/Android.mk   
+    include $(LOCAL_PATH)/mobicore/Android.mk
+    include $(LOCAL_PATH)/macloader/Android.mk
+    include $(LOCAL_PATH)/wifiloader/Android.mk
 endif
